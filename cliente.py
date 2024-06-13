9707e01c684f51c5415222d4c89e147193ed90f0
ValeeeG <90351846+ValeeeG@users.noreply.github.com>
2024-06-13 18:48:47 -0400

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,65 @@
+import socket
+import utils.data_transaction as dt
+from utils.fx import *
+
+def connect_to_bus():
+    # Crear un socket TCP/IP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Conectar el socket al puerto donde el bus está escuchando
+    bus_address = ('localhost', 5000)
+    print('connecting to {} port {}'.format(*bus_address))
+    sock.connect(bus_address)
+    return sock
+
+
+def main_menu():
+    while True:
+        # Menú principal de selección de servicios
+        print("\nSeleccione un servicio:")
+        print("1. Gestión de Usuarios, check")
+        print("2. Gestión de Comunidad, check")
+        print("3. Gestion de Foros, check")
+        print("4. Publicar un mensaje en el foro")
+        print("5. Salir")
+        
+        servicio = input("Ingrese el número del servicio: ")
+        
+        if servicio == '1':
+            sock = connect_to_bus()
+            try:
+                gestion_usuarios(sock)
+            finally:
+                print('closing socket')
+                sock.close()
+
+        if servicio == '2':
+            sock = connect_to_bus()
+            try:
+                gestion_comunidad(sock)
+            finally:
+                print('closing socket')
+                sock.close()
+        if servicio == '3':
+            sock = connect_to_bus()
+            try:
+                gestion_foros(sock)
+            finally:
+                print('closing socket')
+                sock.close()
+        if servicio == '4':
+            sock = connect_to_bus()
+            try:
+                gestion_msn(sock)
+            finally:
+                print('closing socket')
+                sock.close()
+        
+        elif servicio == '5':
+            print("Saliendo del programa.")
+            break
+
+        else:
+            print("Opción no válida. Inténtelo de nuevo.")
+
+if __name__ == "__main__":
+    main_menu()
